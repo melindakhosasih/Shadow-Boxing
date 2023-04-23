5add5d07c40393660c52c6115ab1897a8fc25785
Nerazim <craig19990517@gmail.com>
2023-04-23 23:17:47 +0800

    add hit effect

--- Assets/Nerazim/Script/Hitdetect.cs
+++ Assets/Nerazim/Script/Hitdetect.cs
@@ -2,18 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Hitdetect : MonoBehaviour
 {
+    public RawImage rawImage;
+    public float fadeSpeed = 1;
+    private float imageAlpha = 20f;
+    public RectTransform rectTransform;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+    
+        rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
     }
 
     // Update is called once per frame
     void Update()
     {
+        rawImage.color =  Color.Lerp(rawImage.color, Color.clear, Time.deltaTime * fadeSpeed * 0.5f);
         
     }
 
@@ -24,6 +31,7 @@ public class Hitdetect : MonoBehaviour
         {
             GameObject.FindWithTag("Enemy").GetComponent<DodgeAnimator>().gotHit = true;
             print("Hit Body");
+            rawImage.color = Color.red;
         }
     }
 
@@ -34,6 +42,7 @@ public class Hitdetect : MonoBehaviour
         {
             GameObject.FindWithTag("Enemy").GetComponent<DodgeAnimator>().gotHit = true;
             print("Hit Body");
+            rawImage.color = Color.red;
         }
     }
 }
