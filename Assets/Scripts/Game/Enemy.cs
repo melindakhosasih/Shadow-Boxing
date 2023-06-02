a363171f90cc2f2c298621d3f322a73bcb13c11c
melindakhosasih <melindakhosasih@gmail.com>
2023-06-02 20:38:11 +0800

    fix trigger

--- Assets/Scripts/Game/Enemy.cs
+++ Assets/Scripts/Game/Enemy.cs
@@ -17,51 +17,17 @@ public class Enemy : MonoBehaviour
         
     }
 
-    void OnCollisionEnter(Collision collider) {
-        if (gameObject.tag == "Enemy_LeftHand" && !gameManager.isEnemyLeftPunch()) {
-            // if(collider.collider.tag == "Player") {
-            if(collider.collider.tag == "Body") {
-                // print("enemy hit player");
-                gameManager.setEnemyLeftPunch(true);
-                gameManager.updateEnemyScore(1);
-            }
-        } else if(gameObject.tag == "Enemy_RightHand" && !gameManager.isEnemyRightPunch()) {
-            // if(collider.collider.tag == "Player") {
-            if(collider.collider.tag == "Body") {
-                // print("enemy hit player");
-                gameManager.setEnemyRightPunch(true);
-                gameManager.updateEnemyScore(1);
-            }
-        }
-    }
-
-    void OnCollisionExit(Collision collider) {
-        if (gameObject.tag == "Enemy_LeftHand") {
-            // if(collider.collider.tag == "Player") {
-            if(collider.collider.tag == "Body") {
-                // print("enemy hit player");
-                gameManager.setEnemyLeftPunch(false);
-            }
-        } else if(gameObject.tag == "Enemy_RightHand") {
-            // if(collider.collider.tag == "Player") {
-            if(collider.collider.tag == "Body") {
-                // print("enemy hit player");
-                gameManager.setEnemyRightPunch(false);
-            }
-        }
-    }
-
-    // void OnTriggerEnter(Collider other) {
+    // void OnCollisionEnter(Collision collider) {
     //     if (gameObject.tag == "Enemy_LeftHand" && !gameManager.isEnemyLeftPunch()) {
     //         // if(collider.collider.tag == "Player") {
-    //         if(other.GetComponent<Collider>().tag == "Body") {
+    //         if(collider.collider.tag == "Body") {
     //             // print("enemy hit player");
     //             gameManager.setEnemyLeftPunch(true);
     //             gameManager.updateEnemyScore(1);
     //         }
     //     } else if(gameObject.tag == "Enemy_RightHand" && !gameManager.isEnemyRightPunch()) {
     //         // if(collider.collider.tag == "Player") {
-    //         if(other.GetComponent<Collider>().tag == "Body") {
+    //         if(collider.collider.tag == "Body") {
     //             // print("enemy hit player");
     //             gameManager.setEnemyRightPunch(true);
     //             gameManager.updateEnemyScore(1);
@@ -69,17 +35,51 @@ public class Enemy : MonoBehaviour
     //     }
     // }
 
-    // void OnTriggerExit(Collider other) {
+    // void OnCollisionExit(Collision collider) {
     //     if (gameObject.tag == "Enemy_LeftHand") {
     //         // if(collider.collider.tag == "Player") {
-    //         if(other.GetComponent<Collider>().tag == "Body") {
+    //         if(collider.collider.tag == "Body") {
+    //             // print("enemy hit player");
     //             gameManager.setEnemyLeftPunch(false);
     //         }
     //     } else if(gameObject.tag == "Enemy_RightHand") {
     //         // if(collider.collider.tag == "Player") {
-    //         if(other.GetComponent<Collider>().tag == "Body") {
+    //         if(collider.collider.tag == "Body") {
+    //             // print("enemy hit player");
     //             gameManager.setEnemyRightPunch(false);
     //         }
     //     }
     // }
+
+    void OnTriggerEnter(Collider other) {
+        if (gameObject.tag == "Enemy_LeftHand" && !gameManager.isEnemyLeftPunch()) {
+            // if(collider.collider.tag == "Player") {
+            if(other.GetComponent<Collider>().tag == "Body") {
+                // print("enemy hit player");
+                gameManager.setEnemyLeftPunch(true);
+                gameManager.updateEnemyScore(1);
+            }
+        } else if(gameObject.tag == "Enemy_RightHand" && !gameManager.isEnemyRightPunch()) {
+            // if(collider.collider.tag == "Player") {
+            if(other.GetComponent<Collider>().tag == "Body") {
+                // print("enemy hit player");
+                gameManager.setEnemyRightPunch(true);
+                gameManager.updateEnemyScore(1);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (gameObject.tag == "Enemy_LeftHand") {
+            // if(collider.collider.tag == "Player") {
+            if(other.GetComponent<Collider>().tag == "Body") {
+                gameManager.setEnemyLeftPunch(false);
+            }
+        } else if(gameObject.tag == "Enemy_RightHand") {
+            // if(collider.collider.tag == "Player") {
+            if(other.GetComponent<Collider>().tag == "Body") {
+                gameManager.setEnemyRightPunch(false);
+            }
+        }
+    }
 }
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -17,42 +17,15 @@ public class Player : MonoBehaviour
         
     }
 
-    void OnCollisionEnter(Collision other) {
-        if (gameObject.tag == "LeftHand" && !gameManager.isPlayerLeftPunch()) {
-            if(other.collider.tag == "Enemy") {
-                // print("player hit enemy");
-                gameManager.setPlayerLeftPunch(true);
-                gameManager.updatePlayerScore(1);
-            }
-        } else if(gameObject.tag == "RightHand") {
-            if(other.collider.tag == "Enemy" && !gameManager.isPlayerRightPunch()) {
-                // print("player hit enemy");
-                gameManager.setPlayerRightPunch(true);
-                gameManager.updatePlayerScore(1);
-            }
-        }
-    }
-
-    void OnCollisionExit(Collision other) {
-        if (gameObject.tag == "LeftHand") {
-            if(other.collider.tag == "Enemy") {
-                gameManager.setPlayerLeftPunch(false);
-            }
-        } else if(gameObject.tag == "RightHand") {
-            if(other.collider.tag == "Enemy") {
-                gameManager.setPlayerRightPunch(false);
-            }
-        }
-    }
-    // void OnTriggerEnter(Collider other) {
+    // void OnCollisionEnter(Collision other) {
     //     if (gameObject.tag == "LeftHand" && !gameManager.isPlayerLeftPunch()) {
-    //         if(other.GetComponent<Collider>().tag == "Enemy") {
+    //         if(other.collider.tag == "Enemy") {
     //             // print("player hit enemy");
     //             gameManager.setPlayerLeftPunch(true);
     //             gameManager.updatePlayerScore(1);
     //         }
     //     } else if(gameObject.tag == "RightHand") {
-    //         if(other.GetComponent<Collider>().tag == "Enemy" && !gameManager.isPlayerRightPunch()) {
+    //         if(other.collider.tag == "Enemy" && !gameManager.isPlayerRightPunch()) {
     //             // print("player hit enemy");
     //             gameManager.setPlayerRightPunch(true);
     //             gameManager.updatePlayerScore(1);
@@ -60,15 +33,42 @@ public class Player : MonoBehaviour
     //     }
     // }
 
-    // void OnTriggerExit(Collider other) {
+    // void OnCollisionExit(Collision other) {
     //     if (gameObject.tag == "LeftHand") {
-    //         if(other.GetComponent<Collider>().tag == "Enemy") {
+    //         if(other.collider.tag == "Enemy") {
     //             gameManager.setPlayerLeftPunch(false);
     //         }
     //     } else if(gameObject.tag == "RightHand") {
-    //         if(other.GetComponent<Collider>().tag == "Enemy") {
+    //         if(other.collider.tag == "Enemy") {
     //             gameManager.setPlayerRightPunch(false);
     //         }
     //     }
     // }
+    void OnTriggerEnter(Collider other) {
+        if (gameObject.tag == "LeftHand" && !gameManager.isPlayerLeftPunch()) {
+            if(other.GetComponent<Collider>().tag == "Enemy") {
+                // print("player hit enemy");
+                gameManager.setPlayerLeftPunch(true);
+                gameManager.updatePlayerScore(1);
+            }
+        } else if(gameObject.tag == "RightHand") {
+            if(other.GetComponent<Collider>().tag == "Enemy" && !gameManager.isPlayerRightPunch()) {
+                // print("player hit enemy");
+                gameManager.setPlayerRightPunch(true);
+                gameManager.updatePlayerScore(1);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (gameObject.tag == "LeftHand") {
+            if(other.GetComponent<Collider>().tag == "Enemy") {
+                gameManager.setPlayerLeftPunch(false);
+            }
+        } else if(gameObject.tag == "RightHand") {
+            if(other.GetComponent<Collider>().tag == "Enemy") {
+                gameManager.setPlayerRightPunch(false);
+            }
+        }
+    }
 }
