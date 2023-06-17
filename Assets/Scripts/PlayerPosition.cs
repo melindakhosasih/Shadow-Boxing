4d7768ae3ca841d62d6818823e746c32b56bc8cc
melindakhosasih <melindakhosasih@gmail.com>
2023-06-17 21:37:21 +0800

    reset position in new scene

--- Assets/Scripts/PlayerPosition.cs
+++ Assets/Scripts/PlayerPosition.cs
@@ -2,18 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPosition : MonoBehaviour
-{
+public class PlayerPosition : MonoBehaviour{
+    [SerializeField] Transform resetTransform;
+    [SerializeField] GameObject player;
+    [SerializeField] Camera playerHead;
+    private bool isReset = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
-        print("current position " + transform.position);
+        isReset = false;
+        // ResetPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isReset) {
+            ResetPosition();
+            isReset = true;
+        }
+    }
+
+    // [ContextMenu("Reset Position")]
+    public void ResetPosition() {
+        print("reset position");
+        var rotationAngleY = playerHead.transform.rotation.eulerAngles.y - resetTransform.rotation.eulerAngles.y;
+        player.transform.Rotate(0, -rotationAngleY, 0);
+
+        var distanceDiff = resetTransform.position - playerHead.transform.position;
+        Vector3 pos = player.transform.position += distanceDiff;
+        pos.y = 0;
+        player.transform.position = pos;
     }
 }
